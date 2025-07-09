7828624c83c153176b2ab3adca93d6f4a5310b90
Tharaka_Madhusanka <tharakalearn@gmail.com>
2025-07-09 08:27:12 +0800

    Added Du;licateNameValidatorAntiPatternApproachOne

--- projects/form-control-touched-mystery/src/app/control-list/control-list.component.ts
+++ projects/form-control-touched-mystery/src/app/control-list/control-list.component.ts
@@ -14,6 +14,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { first } from 'rxjs';
 
 export interface WeirdozFormGroupModel {
   fieldsArray: FormArray<FormGroup<{ name: FormControl<string> }>>;
@@ -57,7 +58,7 @@ export class ControlListComponent implements OnInit {
   addControl() {
     const group = this.fb.group<{ name: FormControl<string> }>({
       name: this.fb.control<string>('', {
-        validators: [this.duplicateNameValidator()],
+        validators: [this.duplicateNameValidatorAntiPatternApproachOne()],
         // When to run the validation
         // 'change' - on every change
         // 'blur' - when the control loses focus
@@ -66,6 +67,12 @@ export class ControlListComponent implements OnInit {
       }),
     });
     this.weirdozFg.controls.fieldsArray.push(group, { emitEvent: false });
+
+    // Approach 1 - Mark as touched explicitly
+    group.controls.name.valueChanges.subscribe((_) => {
+      group.controls.name.markAsTouched();
+      group.controls.name.updateValueAndValidity();
+    });
   }
 
   duplicateNameValidator(): ValidatorFn {
@@ -77,6 +84,39 @@ export class ControlListComponent implements OnInit {
       console.log(`isControlTouched ${control.touched}`);
 
       if (!control.value) {
+        // If the control is not touched or has no value, skip validation
+        return null;
+      }
+
+      console.log('Validator reached to functional area');
+
+      // Check if the control has a value
+      const name = control.value;
+      const isDuplicate = this.fieldsArray.controls.filter((group) => {
+        return group.get('name')?.value === name;
+      });
+      return isDuplicate.length > 1 ? { duplicateName: true } : null;
+    };
+  }
+
+  // By following the Anti Pattern to check Form Control Touched state before
+  // executes the validation
+  // by following the anti pattern, we can expect the validator is not working as expected
+  // This issue, we can fix using following approaches, which makes the code to handle unnecessary operations
+  // 1. Using Value Changes and mark the field touched on first change
+  // 2. Using Async Validator and NgOnStable
+
+  // Approach 1 - Use Value Changes, update the touched state explicitly
+  duplicateNameValidatorAntiPatternApproachOne(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      console.log(`Run count ${++this.runcount}`);
+      // isDirty
+      console.log(`isControlDirty ${control.dirty}`);
+      // isTouched
+      console.log(`isControlTouched ${control.touched}`);
+
+      // Anti-pattern, to check Touched State inside the validator Func
+      if (!control.touched || !control.value) {
         // If the control is not touched or has no value, skip validation
         return null;
       }
